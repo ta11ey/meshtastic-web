aa0d695bf3252082faf25efc4ee1315fb32d30bc
Charles Crossan <crossan007@gmail.com>
2020-11-13 22:53:52 -0500

    Working proof of concept with meshtastic radio communication

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Client, IHTTPConnection,NodeDB,SettingsManager, version} from '@meshtastic/meshtasticjs'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +11,36 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const client = new Client();
+const httpconn =client.createHTTPConnection();
+
+// Set connection params
+let sslActive;
+if (window.location.protocol === 'https:') {
+    sslActive = true;
+} else {
+    sslActive = false;
+}
+let deviceIp = window.location.hostname; // Your devices IP here
+
+
+httpconn.addEventListener("fromRadio", (event) => {
+  console.log(event.detail.toJSON());
+});
+
+httpconn.connect(deviceIp, sslActive)
+.then(result => {
+
+    alert('device has been configured')
+    // This gets called when the connection has been established
+    // -> send a message over the mesh network. If no recipient node is provided, it gets sent as a broadcast
+    return httpconn.sendText('meshtastic is awesome');
+
+})
+.then(result => {
+
+    // This gets called when the message has been sucessfully sent
+    console.log('Message sent!');
+})
+
+.catch(error => { console.log(error); });
