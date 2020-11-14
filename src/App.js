8b1e8ac139d7071a318b4c21b5e04e78efdc7050
Charles Crossan <crossan007@gmail.com>
2020-11-13 23:28:32 -0500

    move meshtastic client into react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Client, IHTTPConnection,NodeDB,SettingsManager, version} from '@meshtastic/meshtasticjs'
 
-function App() {
+class App extends Component  {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    messages: []
+  }
+
+  constructor(props) {
+    super(props);
+    this.setupHTTP();
+  }
+
+  addToMessageArray(newmessage) {
+    this.setState({
+      messages: [...this.state.messages,newmessage]
+    });
+  }
+
+  setupHTTP() { 
+    const client = new Client();
+    const httpconn =client.createHTTPConnection();
+  
+    // Set connection params
+    let sslActive;
+    if (window.location.protocol === 'https:') {
+        sslActive = true;
+    } else {
+        sslActive = false;
+    }
+    let deviceIp = window.location.hostname; // Your devices IP here
+  
+  
+    httpconn.addEventListener("fromRadio", (event) => {
+      console.log(JSON.stringify(event.detail));
+      this.addToMessageArray(event.detail);
+    });
+  
+    httpconn.addEventListener("dataPacket", (event) => {
+      console.log(JSON.stringify(event.detail));
+      this.addToMessageArray(event.detail);
+    });
+  
+    httpconn.addEventListener("userPacket", (event) => {
+      console.log(JSON.stringify(event.detail));
+      this.addToMessageArray(event.detail);
+    });
+  
+  
+    httpconn.addEventListener("positionPacket", (event) => {
+      console.log(JSON.stringify(event.detail));
+      this.addToMessageArray(event.detail);
+    });
+  
+  
+    httpconn.addEventListener("nodeListChanged", (event) => {
+      console.log(JSON.stringify(event.detail));
+      this.addToMessageArray(event.detail);
+    });
+  
+    httpconn.connect(deviceIp, sslActive)
+    .then(result => {
+  
+        alert('device has been configured')
+        // This gets called when the connection has been established
+        // -> send a message over the mesh network. If no recipient node is provided, it gets sent as a broadcast
+        return httpconn.sendText('meshtastic is awesome');
+  
+    })
+    .then(result => {
+  
+        // This gets called when the message has been sucessfully sent
+        console.log('Message sent!');
+    })
+  
+    .catch(error => { console.log(error); });
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        {this.state.messages.map((value, index) => {
+          <pre>{value}</pre>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Client, IHTTPConnection,NodeDB,SettingsManager, version} from '@meshtastic/meshtasticjs'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,37 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-const client = new Client();
-const httpconn =client.createHTTPConnection();
-
-// Set connection params
-let sslActive;
-if (window.location.protocol === 'https:') {
-    sslActive = true;
-} else {
-    sslActive = false;
-}
-let deviceIp = window.location.hostname; // Your devices IP here
-
-
-httpconn.addEventListener("fromRadio", (event) => {
-  console.log(event.detail.toJSON());
-});
-
-httpconn.connect(deviceIp, sslActive)
-.then(result => {
-
-    alert('device has been configured')
-    // This gets called when the connection has been established
-    // -> send a message over the mesh network. If no recipient node is provided, it gets sent as a broadcast
-    return httpconn.sendText('meshtastic is awesome');
-
-})
-.then(result => {
-
-    // This gets called when the message has been sucessfully sent
-    console.log('Message sent!');
-})
-
-.catch(error => { console.log(error); });
