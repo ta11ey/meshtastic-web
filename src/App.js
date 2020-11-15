fb49f90c56457b40d5167181a07a9ce98a9c1cdf
Charles Crossan <crossan007@gmail.com>
2020-11-15 14:01:09 -0500

    move offline sample data to external resource

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Sidebar from './sidebar';
 import Messages from './components/messages';
 import {Client, IHTTPConnection,NodeDB,SettingsManager, version} from '@meshtastic/meshtasticjs'
 import PacketLog from './components/PacketLog';
-
+import SampleData from './SampleData';
 class App extends Component  {
 
 
@@ -94,33 +94,7 @@ class App extends Component  {
   
     .catch(error => { 
       this.setState({
-        messages:[...this.state.messages,{
-          "from":476491748,
-          "to":4294967295,
-          "decoded": {
-            "data": {
-              "typ":1,
-              "payload":"aGV5b29vbw=="
-            }
-          },
-          "id":3698181789,
-          "rxSnr":11,
-          "rxTime":160540164,
-          "hopLimit":1},
-          {
-            "from":476493745,
-            "to":4294967294,
-            "decoded": {
-              "data": {
-                "typ":1,
-                "payload":"aGV5b29vbw=="
-              }
-            },
-            "id":3698181789,
-            "rxSnr":11,
-            "rxTime":1605401643,
-            "hopLimit":1}
-        ]
+        messages: SampleData.messages
       })
       console.log(error); });
   }
---
+++ src/SampleData.js
@@ -0,0 +1,31 @@
+const SampleData = {
+  messages:[{
+    "from":476491748,
+    "to":4294967295,
+    "decoded": {
+      "data": {
+        "typ":1,
+        "payload":"aGV5b29vbw=="
+      }
+    },
+    "id":3698181789,
+    "rxSnr":11,
+    "rxTime":160540164,
+    "hopLimit":1},
+    {
+      "from":476493745,
+      "to":4294967294,
+      "decoded": {
+        "data": {
+          "typ":1,
+          "payload":"aGV5b29vbw=="
+        }
+      },
+      "id":3698181789,
+      "rxSnr":11,
+      "rxTime":1605401643,
+      "hopLimit":1}
+  ]
+};
+
+export default SampleData;
