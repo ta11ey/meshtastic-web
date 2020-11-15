d1124d6ded3dc135d1b30fb756fe963df896de9e
Charles Crossan <crossan007@gmail.com>
2020-11-14 19:55:09 -0500

    cleanup dispaly of a message

--- src/components/message.js
+++ src/components/message.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './message.css';
 
 class Message extends Component  {
-
+  sentByUs() { 
+    return this.props.message.from == "476493745";
+  }
   render() { 
     return (
       <div 
         className="Message" 
         style= {{
-          "background-color": (this.props.message.from == "476493745" ?  'red' : 'blue' )
+          "background-color": (this.sentByUs() ?  'red' : 'blue' ),
+          "float":  (this.sentByUs() ?  'left' : 'right' )
          }} >
          {JSON.stringify(this.props.message)}
       </div>
