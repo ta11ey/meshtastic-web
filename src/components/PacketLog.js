5d0b8b5de248fc88fe688587aea74bcfaab3ede5
Charles Crossan <crossan007@gmail.com>
2020-11-27 22:15:08 -0500

    deal with unrenderable packets

--- src/components/PacketLog.js
+++ src/components/PacketLog.js
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 import "./messages.css";
 
 class PacketLog extends Component {
+  // TODO: This shouldn't be necessary
+  // but for some reason, packets from meshtastic.js throw
+  // an exception when stringifying them
+  PacketToString(value) { 
+    try {
+      return JSON.stringify(value);
+    }
+    catch(error) {
+      return "Couldn't render packet";
+    }
+  }
   render() {
     console.log(this.props.packets);
     return (
@@ -9,11 +20,11 @@ class PacketLog extends Component {
         <div className="MessageHistory">
           Packets:
           {this.props.packets.map((value, index) => (
-            <div style={{
+            <div key="packet${index}" style={{
               marginTop: 10,
               border: "1px solid black"
             }}>
-              {JSON.stringify(value)}
+              {this.PacketToString(value)}
               <br />
             </div>
           ))}
