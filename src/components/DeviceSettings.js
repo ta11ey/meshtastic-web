62d019daf40fd512a43912226cbb6b303c965633
Charles Crossan <crossan007@gmail.com>
2020-12-12 14:27:32 -0500

    dump settings keys instead of hardcoding

--- src/components/DeviceSettings.js
+++ src/components/DeviceSettings.js
@@ -3,33 +3,43 @@ import "./DeviceSettings.css";
 
 class DeviceSettings extends Component {
   render() {
+
+    const prefs = Object.keys(this.props.settings.radioConfig.preferences).map(key => 
+      <div>
+        <span className="settingLabel">{key} </span>
+       <span className="settingValue">{this.props.settings.radioConfig.preferences[key]}</span><br/>
+       </div>
+    )
+
+    const myInfo = Object.keys(this.props.settings.myInfo).map(key => 
+      <div>
+        <span className="settingLabel">{key} </span>
+       <span className="settingValue">{this.props.settings.myInfo[key]}</span><br/>
+       </div>
+    )
+
+
+    const channelSettings = Object.keys(this.props.settings.radioConfig.channelSettings).map(key => 
+      <div>
+        <span className="settingLabel">{key} </span>
+       <span className="settingValue">{this.props.settings.radioConfig.channelSettings[key]}</span><br/>
+       </div>
+    )
+
     return (
       <div className="DeviceSettings">
         <div className="DeviceProfile">
           <span className="SectionHeader">Device Profile</span><br/>
-          <span className="settingLabel">Node Number: </span>
-          <span className="settingValue">{this.props.settings.myInfo.myNodeNum}</span><br/>
-          <span className="settingLabel">Hardware Model: </span>
-          <span className="settingValue">{this.props.settings.myInfo.hwModel}</span><br/>
-          <span className="settingLabel">Availalable Channels: </span>
-          <span className="settingValue">{this.props.settings.myInfo.numChannels}</span><br/>
-          <span className="settingLabel">Has GPS: </span>
-          <span className="settingValue">{this.props.settings.myInfo.hasGps.toString()}</span><br/>
-          <span className="settingLabel">Firmware Version: </span>
-          <span className="settingValue">{this.props.settings.myInfo.firmwareVersion}</span><br/>
+          { myInfo }
         </div>
         <div className="UserPreferences">
         <span className="SectionHeader">User Preferences</span><br/>
-          <span className="settingLabel">wifiSsid </span>
-          <span className="settingValue">{this.props.settings.radioConfig.preferences.wifiSsid}</span><br/>
+          {prefs}
         </div>
         
         <div className="ChannelSettings">
         <span className="SectionHeader">Channel Settings</span><br/>
-          <span className="settingLabel">Channel Name: </span>
-          <span className="settingValue">{this.props.settings.radioConfig.channelSettings.name}</span><br/>
-          <span className="settingLabel">Modem Config: </span>
-          <span className="settingValue">{this.props.settings.radioConfig.channelSettings.modemConfig}</span><br/>
+         { channelSettings }
 
 
         </div>
